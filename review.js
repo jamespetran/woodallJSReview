69b1329c13416ca354614d24fd5ac96bebdc19b6
James Petran <jm.ptrn@gmail.com>
2021-12-06 11:36:19 -0600

    parameters and jargon

---
+++ review.js
@@ -0,0 +1,72 @@
+/* this is a block comment */
+/* 
+Jargon!!
+
+.  - DOT
+() - parenthesis
+[] - square brackets/braces
+{} - curly brackets/braces
+
+<   - less than
+>   - greater than
+    ALSO CALLED angle brackets in html
+
+/   - slash, forward slash, whack
+\   - backslash
+
+!   - exclamation point, bang, not
+#   - hashtag, pound, octothorp
+*   - star, asterisk
+
+||  - or operator, pipes
+&&  - ampersands, and operator
+$   - bling, cash money, jQuery
+NaN - not a number, bread🍞
+
+*/
+
+/*
+Primitive datatypes
+
+    Strings     'tacos'
+    Numbers     12345
+    Boolean     true/false
+    Undefined   undefined
+    Null        null
+
+Complex datatypes
+
+    Arrays      [1,2,3,4,5], ["hello", "world"]
+    Objects     { firstName: "James" }
+
+*/
+
+let nothing = null;
+
+// 🐫 always use camelCase
+let firstName = "James"
+
+// KEBAB Case // commonly used in html
+// <div class="first-name">James </div>
+
+// Other naming conventions
+// snake_case
+// kebab-case
+// PascalCase or UpperCamelCase
+
+let person = {
+    firstName: "James"
+    lastName: "Petran"
+    age: 31
+}
+
+/**
+ * Expressions
+ * 
+ * Operators
+ * 
+ */
+//an expression evaluates to a single value
+7 + 3; //10
+123456
+    
